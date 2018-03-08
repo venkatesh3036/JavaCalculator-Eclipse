08bad11e1f6911429c47a357d4c5f52117eccc31
Ravi <ravi2krishna@gmail.com>
2018-03-08 08:38:37 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -38,10 +38,6 @@ public class Calculator {
 		return first*second;
 	}
 	
-	public long divFucn(long first, long second){
-		
-		return first/second;
-	}
 	public static void main(String[] args) {
 		
 		Calculator cc = new Calculator();
